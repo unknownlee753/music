9d1c2920438726d68675fafc867112efbf4bb4fe
unknownlee753 <90024284+unknownlee753@users.noreply.github.com>
2021-09-08 19:34:12 +0530

    Update helper.py

--- plugins/bot/helper.py
+++ plugins/bot/helper.py
@@ -236,7 +236,7 @@ async def start(client, message):
             ]
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    m=await message.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    m=await message.reply_photo(photo="https://telegra.ph/file/6264be29a934705b713b3.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
     await mp.delete(m)
     await mp.delete(message)
 
@@ -263,6 +263,6 @@ async def help(client, message):
     reply_markup = InlineKeyboardMarkup(buttons)
     if msg.get('help') is not None:
         await msg['help'].delete()
-    msg['help'] = await message.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption=HELP_TEXT, reply_markup=reply_markup)
+    msg['help'] = await message.reply_photo(photo="https://telegra.ph/file/6264be29a934705b713b3.jpg", caption=HELP_TEXT, reply_markup=reply_markup)
     await mp.delete(message)
 
