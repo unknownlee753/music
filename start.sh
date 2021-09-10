0fe6d23597195009d34c616f1445e7ac2f524185
unknownlee753 <90024284+unknownlee753@users.noreply.github.com>
2021-09-10 20:47:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
-cd /RadioPlayerV3
+git clone https://github.com/unknownlee753/music.git /music
+cd /music
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
